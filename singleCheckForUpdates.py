ad26ebdd84634ac6de25ce311bf2dc9b20237b04
Blake Rainwater <rainwaterblake@gmail.com>
2023-08-07 20:46:20 -0400

    Add windows support

--- singleCheckForUpdates.py
+++ singleCheckForUpdates.py
@@ -13,12 +13,26 @@ import paho.mqtt.client as mqtt
 UPDATE_DELAY = 60*30
 
 secrets_path = "/etc/homeassistant/secrets.json"
+directory = os.path.dirname(__file__)
+second_secrets_path = os.path.join(directory, "secrets.json")
+secrets = None
+for path in [secrets_path, second_secrets_path]:
+    if os.path.isfile(path):
+        with open(path) as sfile:
+            secrets = json.load(sfile)
+        break
+if secrets is None:
+    raise SystemExit("Error! No file found at " + secrets_path + " or " + second_secrets_path + " unable to send!")
 
-if not os.path.isfile(secrets_path):
-    raise SystemExit("Error! No file found at " + secrets_path + " unable to send!")
-
-with open(secrets_path) as sfile:
-    secrets = json.load(sfile)
+def numWindowsUpdate():
+    out = subprocess.check_output(["PowerShell", "Get-WindowsUpdate", "-AutoSelectOnWebSites"])
+    lines = out.split("\r\n".encode("ascii"))
+    print(lines)
+    numupdates = len(lines) - 6
+    print(numupdates)
+    if numupdates < 0:
+        numupdates = 0
+    return numupdates
 
 def init_mqtt_client(username, password, broker, port):
     mqtt_client = mqtt.Client(socket.gethostname())
@@ -69,9 +83,12 @@ mqtt_client = init_mqtt_client(
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 blarg_path = os.path.join(dir_path, "blarg.py")
-apt_check_output = subprocess.check_output(blarg_path, stderr=subprocess.STDOUT).decode('utf-8')
-output_lst = apt_check_output.split(";")
-num_updates, num_security = int(output_lst[0]), output_lst[1]
+try:
+    apt_check_output = subprocess.check_output(blarg_path, stderr=subprocess.STDOUT).decode('utf-8')
+    output_lst = apt_check_output.split(";")
+    num_updates, num_security = int(output_lst[0]), output_lst[1]
+except OSError:
+    num_updates = numWindowsUpdate()
 #print("Num updates: " + str(num_updates))
 #print("Num security: " + str(num_security))
 
